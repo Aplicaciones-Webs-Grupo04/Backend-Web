f2ce51c7cbd88f26481c99bedfbe60cda587b8c7
RominaMaita <u202213765@upc.edu.pe>
2024-11-01 18:25:49 -0500

    chore: add model builder extensions and string extensions.

---
+++ psymed-platform/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace psymed_platform.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class StringExtensions {
+
+    public static string ToSnakeCase(this string text) {
+        //TODO: Add To SnakeCase function
+    }
+
+    public static string ToPlural(this string text) {
+        //TODO: Add To Plural function
+    }
+    
+}
