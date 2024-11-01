d2b4b688fc53c4ddf48d2dea219b1237277c77ee
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-10-31 19:14:52 -0500

    Add controller to Appoiment
    Controller to Appoiment is added

---
+++ psymed-platform/WebApi/Controllers/AppointmentController.cs
@@ -0,0 +1,13 @@
+﻿namespace psymed_platform.Appointment_Managment_Bc.WebApi.Controllers;
+
+public class AppointmentController
+{
+using Microsoft.AspNetCore.Mvc;
+using psymed_platform.Appointment_Managment_Bc.Application.Services;
+using psymed_platform.Appointment_managment.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace
+}
