e3837707c392736028ca37be49aced45bbfdce99
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-10-31 19:33:47 -0500

    model to appoinment is added
    the model is added to the appoinment

---
+++ psymed-platform/Appointment_Managment_Bc/Domain/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace psymed_platform.Appointment_managment.Domain.Models;
+
+public class Appointment {
+    public int Id { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string Patient { get; set; }
+    public string Asunto { get; set; }
+
+    public Appointment(int id, DateTime appointmentDate, string patient, string asunto) {
+        Id = id;
+        AppointmentDate = appointmentDate;
+        Patient = patient;
+        Asunto = "therapy";
+    }
+}
