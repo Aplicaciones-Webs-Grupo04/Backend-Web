306ccd9e4e1796f69ea01d7dbdc8a92134fa6315
RominaMaita <u202213765@upc.edu.pe>
2024-11-02 15:59:47 -0500

    feat(medication): add medication value objects.

---
+++ psymed-platform/Medication/Domain/Model/ValueObjects/MedicationFrequency.cs
@@ -0,0 +1,6 @@
+﻿namespace psymed_platform.Medication.Domain.Model.ValueObjects;
+
+public record MedicationFrequency(string Frecuency)
+{
+    public MedicationFrequency(): this(string.Empty){}
+};
