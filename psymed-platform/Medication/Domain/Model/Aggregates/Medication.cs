0b2b96670ea8514d95d02a374a2e20fb035331e1
RominaMaita <u202213765@upc.edu.pe>
2024-11-02 12:19:34 -0500

    feat(medication): add medication aggregate and value object.

---
+++ psymed-platform/Medication/Domain/Model/Aggregates/Medication.cs
@@ -0,0 +1,10 @@
+﻿namespace psymed_platform.Medication.Domain.Model.Aggregates;
+
+public class Medication {
+    
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Frecuency { get; private set; }
+    public int Quantity { get; private set; }
+    
+}
