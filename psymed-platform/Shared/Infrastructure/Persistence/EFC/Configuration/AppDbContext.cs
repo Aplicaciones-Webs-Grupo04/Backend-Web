fe6116543fb3f70fc1416d7884031a55eb538bb9
RominaMaita <u202213765@upc.edu.pe>
2024-11-02 11:54:44 -0500

    chore: added basic shared items.

---
+++ psymed-platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace psymed_platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options) {
+    
+    
+}
