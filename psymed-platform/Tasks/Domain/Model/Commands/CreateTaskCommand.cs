fdac42cd6221f24f092ad63b217b3b6e81b698c7
JoseRiega <u202211254@upc.edu.pe>
2024-11-03 12:19:12 -0500

    feat: Create CreateTaskCommand.cs

---
+++ psymed-platform/Tasks/Domain/Model/Commands/CreateTaskCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace PsymedPlatform.Tasks.Domain.Model.Commands
+{
+    public class CreateTaskCommand
+    {
+        public int IdPatient { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+    }
+}
