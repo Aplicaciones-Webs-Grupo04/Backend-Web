4190b6cbb3dfbeaf6107b09364975dfad7e77992
RominaMaita <u202213765@upc.edu.pe>
2024-11-04 17:34:11 -0500

    feat(medication): add base template repositories in shared.

---
+++ psymed-platform/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using psymed_platform.Shared.Domain.Repositories;
+using psymed_platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace psymed_platform.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public async Task CompleteAsync() => await context.SaveChangesAsync();
+}
